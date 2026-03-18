124268b620107f3224c2c2a1223a2866f1632583
Ari Palo <679146+aripalo@users.noreply.github.com>
2026-03-18 15:43:42 +0200

    feat: initial setup

--- .projenrc.ts
+++ .projenrc.ts
@@ -1,16 +1,50 @@
-import { cdk } from 'projen';
+import { cdk, javascript } from 'projen';
+import { UpdateSnapshot } from 'projen/lib/javascript';
+
+const JSII_VERSION = "~5.9.0";
+const JEST_VERSION = "^30";
+
 const project = new cdk.JsiiProject({
-  author: 'Ari Palo',
-  authorAddress: '679146+aripalo@users.noreply.github.com',
+  name: "@alma-cdk/construct-library",
+  author: "Alma Media",
+  authorAddress: "opensource@almamedia.dev",
+  description: "Projen custom project to build Alma CDK construct libraries",
+  repositoryUrl: 'https://github.com/alma-cdk/construct-library.git',
   defaultReleaseBranch: 'main',
-  jsiiVersion: '~5.9.0',
-  name: 'base',
+  releaseEnvironment: "production",
   projenrcTs: true,
-  repositoryUrl: 'https://github.com/679146+aripalo/base.git',
+  projenCommand: "pnpm exec projen",
+  packageManager: javascript.NodePackageManager.PNPM,
+  npmAccess: javascript.NpmAccess.PUBLIC,
+  npmTrustedPublishing: true,
+  jsiiVersion: JSII_VERSION,
+  jestOptions: {
+    jestVersion: JEST_VERSION,
+    updateSnapshot: UpdateSnapshot.NEVER,
+  },
 
   // deps: [],                /* Runtime dependencies of this module. */
   // description: undefined,  /* The description is just a string that helps people understand the purpose of the package. */
   // devDeps: [],             /* Build dependencies for this module. */
   // packageName: undefined,  /* The "name" in package.json. */
 });
+
+project.addDevDeps("typescript@^5.9"); // Defaults to very old typescript@4.9
+project.addDevDeps("@types/semver@^7");
+project.addDevDeps("json-schema-to-typescript@^15");
+
+project.addBundledDeps("zod@4");
+project.addBundledDeps("semver@7");
+
+project.addTask("format", {
+  exec: "prettier --write .",
+});
+
+// project.addTask("generate:pnpm-workspace-types", {
+//   description:
+//     "Download pnpm-workspace JSON schema and generate pnpm-workspace-schema.d.ts",
+//   exec: "ts-node scripts/generate-pnpm-workspace-types.ts",
+// });
+
+
 project.synth();
